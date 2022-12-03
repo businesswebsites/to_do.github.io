8dbf954ef5ebc7684f6b41766b41210b8cd51207
businesswebsites <droese.websites@gmail.com>
2022-12-03 12:05:30 +0100

    To-Do App First Commit
    Not Perfect only create new todo
    no delete function
    no edit function

---
+++ code.js
@@ -0,0 +1,49 @@
+function add_task(){
+    let input_value = document.getElementById("to_do").value;
+    const newDiv = document.createElement("div");
+    newDiv.style.display = "flex";
+    newDiv.style.justifyContent = "space-between";
+    newDiv.style.backgroundColor = "#111827";
+    newDiv.style.padding = "1rem";
+    newDiv.style.borderRadius = "1rem";
+    newDiv.style.marginBottom = "1rem";
+
+    const newContent = document.createTextNode(input_value);
+    newDiv.appendChild(newContent);
+    const currentDiv = document.getElementById("new_task");
+    currentDiv.after(newDiv);
+
+
+    const child_div_bearbeiten = document.createElement("button");
+    child_div_bearbeiten.setAttribute("id", "button_edit");
+    child_div_bearbeiten.style.borderRadius = "1rem"
+    child_div_bearbeiten.style.float = "right";
+    child_div_bearbeiten.style.marginRight = "0";
+    child_div_bearbeiten.onclick = function (){
+        alert("Bearbeiten");
+    }
+    const bearbeiten = document.createTextNode("Bearbeiten");
+    child_div_bearbeiten.appendChild(bearbeiten);
+    newDiv.appendChild(child_div_bearbeiten)
+
+    const child_div_delete = document.createElement("button");
+    child_div_delete.style.borderRadius = "1rem";
+    //child_div_delete.style.float = "right";
+    child_div_delete.onclick = function (){
+        currentDiv.removeChild(newDiv);
+    }
+    const delete_el = document.createTextNode("Löschen");
+    child_div_delete.appendChild(delete_el);
+    newDiv.appendChild(child_div_delete);
+
+
+
+    /*
+    let child_div_bearbeiten = document.createElement("div");
+    child_div_bearbeiten.appendChild(bearbeiten);
+    const bearbeiten = document.createTextNode("Bearbeiten");
+    bearbeiten.style.backgroundImage = "linear-gradient(to right, #EC4899, #8B5CF6)";
+    newDiv.after(child_div_bearbeiten);*/
+
+
+}
